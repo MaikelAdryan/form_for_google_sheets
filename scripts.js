b9d86b59da18b034ab44d97fda236195c66c8eff
adryanmaikel <adryanmaikel@gmail.com>
2023-09-19 16:15:44 -0300

    finalizando projeto

--- scripts.js
+++ scripts.js
@@ -4,13 +4,17 @@ const handleSubmit = (events) => {
   const name = document.querySelector("input[name=name]").value
   const email = document.querySelector("input[name=email]").value
 
+  const DATA = {
+    name, email
+  }
+
   fetch("https://api.sheetmonkey.io/form/kHncNXsfYivg844Bipay4d",{
     method: 'POST',
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ name, email })
-  })
+    body: JSON.stringify(DATA)
+  }).then(()=>{ alert("dados salvos") })
 }
 document.querySelector("form").addEventListener("submit", handleSubmit)
